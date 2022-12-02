808fd368e1d5e80f844173c9ff557fcac2afb334
khalid <khalidcomilla58@gmail.com>
2022-12-03 00:29:07 +0600

    first commit

---
+++ Exercise1.py
@@ -0,0 +1,16 @@
+# Problem 1: we can't create a variable called break. Because it is a keyword.
+# Problem 2:
+birthYear = 2000
+currYear = 2022
+
+age = currYear - birthYear
+print(age)
+
+# Problem 3:
+firstName = 'Mohammad '
+middleName = 'khalid '
+lastName = 'abdullah'
+fullName = firstName + middleName + lastName
+print(fullName)
+
+# Problem 4: record-one and record^one are invalid
