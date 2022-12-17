a54e9a3127d73b88eeebff420e886f0f01c7047d
khalid <khalidcomilla58@gmail.com>
2022-12-17 13:14:07 +0600

    second commit

--- string.py
+++ string.py
@@ -4,3 +4,7 @@ print("this is 0 to 3: " + text[0:3])  # prints from 0th index to 2nd index not 
 print("this is 4 to 9: " + text[4:9])  # prints from 4th index to 2nd index not 8th index (before 9th)
 print("this is 4 to end: " + text[4:])   # prints from 4th index to the last index
 print("this is beginning to 3: " + text[:3])   # prints from the beginning to 2nd (before 3rd)
+# we can't make changes like text[3] = 'a' as we can do in cpp . It will give us error
+text = '''this will store multiline 
+string. To store multiline string we have to use triple quotes.'''
+print(text)
