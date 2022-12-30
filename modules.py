5de44e4d5c4387aac661c5894b4ce67bf3768cfc
khalid <khalidcomilla58@gmail.com>
2022-12-30 18:42:36 +0600

    25th commit(comment added)

--- modules.py
+++ modules.py
@@ -5,7 +5,7 @@ import calendar
 # user defined library
 import Function as f
 area, circum, diameter = f.circle_calc(5)
-print(round(area, 2))
+print(round(area, 2))  # 78.54 (calculated from the Function.py file
 
 # if you want to import from a different directory
 # import  sys
