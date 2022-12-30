3b2f04022cbae2ae587e0f6298549d55c117350b
khalid <khalidcomilla58@gmail.com>
2022-12-30 12:02:04 +0600

    21st commit

--- dictionary and list.py
+++ dictionary and list.py
@@ -1,12 +1,15 @@
-stockPrices = {"info": [600, 630, 620], "ril": [1430, 1490, 1567], "mtl": [234, 180, 160]}
+import statistics
+
+stockPrices = {
+    "info": [600, 630, 620],
+    "ril": [1430, 1490, 1567],
+    "mtl": [234, 180, 160]
+}
 q = int(input("Enter your query(1 for printing stock prices and 2 for adding stockPrice: "))
 sm = 0
 if q == 1:
-    for k in stockPrices:
-        sm = 0
-        for i in stockPrices[k]:
-            sm += i
-        print(f"{k} ==> {stockPrices[k]} ==> avg = {sm/len(stockPrices[k])}")
+    for stock, pricelist in stockPrices.items():
+        print(f"{stock} ==> {pricelist} ==> avg = {round(statistics.mean(pricelist),2)}")
 else:
     stock = input("Enter the name of the stock you want to input: ")
     value = input("Enter the value of the stock")
