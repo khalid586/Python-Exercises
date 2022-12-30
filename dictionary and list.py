e8f589ecb8f45b1ffed0683fba7a267db725596c
khalid <khalidcomilla58@gmail.com>
2022-12-30 11:50:00 +0600

    19Th commit

--- dictionary and list.py
+++ dictionary and list.py
@@ -1,9 +1,9 @@
 stockPrices = {"info": [600, 630, 620], "ril": [1430, 1490, 1567], "mtl": [234, 180, 160]}
-q = int(input("Enter your query(1 for printing stockprices and 2 for adding stockPrice: "))
+q = int(input("Enter your query(1 for printing stock prices and 2 for adding stockPrice: "))
 sm = 0
 if q == 1:
     for k in stockPrices:
         sm = 0
         for i in stockPrices[k]:
             sm += i
-        print(f"{k} ==> avg = {sm/len(stockPrices[k])}")
+        print(f"{k} ==> {stockPrices[k]} ==> avg = {sm/len(stockPrices[k])}")
